7ce5c6557dff26d994f6c5bb0f139c8035fa0359
Felix_ZS <felix.zs123@googlemail.com>
2014-12-07 18:41:24 +0100

    r scripts and pngs added

---
+++ plot3.R
@@ -0,0 +1,38 @@
+# NOTE: set working directory to project root directory, e.g.:
+# setwd("~/R/ExData_Plotting1")
+
+# download and unzip dataset
+if (!file.exists("data/household_power_consumption.txt")) {
+  dir.create("data")
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+                "data/exdata-data-household_power_consumption.zip",
+                method = "auto")
+  unzip("data/exdata-data-household_power_consumption.zip", exdir = "data")  
+}
+
+# temporarily switch locale to avoid region-specific datetime formatting issues
+lct <- Sys.getlocale("LC_TIME")
+Sys.setlocale("LC_TIME", "C")
+
+# read and subset data
+data <- read.table("data/household_power_consumption.txt", header = TRUE, sep = ";",
+                   na.strings = "?", stringsAsFactors = FALSE)
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+df <- subset(data, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+# add new combined datetime feature
+df$dt <- strptime(paste(as.character(df$Date), df$Time), "%Y-%m-%d %H:%M:%S")
+
+# create plot
+png("plot3.png")
+plot(df$dt, df$Sub_metering_1, type = "l", main = "", xlab = "",
+     ylab = "Energy sub metering")
+lines(df$dt, df$Sub_metering_2, col = "red")
+lines(df$dt, df$Sub_metering_3, col = "blue")
+legend("topright", 
+       lty = 1,
+       col = c("black", "red", "blue"), 
+       legend = names(df)[7:9])
+dev.off()
+
+# set locale back to default
+Sys.setlocale("LC_TIME", lct)
